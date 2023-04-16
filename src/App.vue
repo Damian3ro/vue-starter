afece08684d4fdd928068e80057313a293d4c88c
Damian Stasik <dstasik@student.agh.edu.pl>
2023-04-16 14:27:16 +0200

    Log in and out functionalities added

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,45 @@
 <template>
   <div class="my-app">
-    <h1>Hello MWO!</h1>
+    <h1>Witaj w systemie do zapisów na zajęcia</h1>
+    <div v-if="loggedIn">
+      <h2>Witaj {{ email }}</h2>
+<!--      <button @click="logOut()">Wyloguj</button>-->
+      <a @click="logOut()">Wyloguj</a>
+    </div>
+    <div v-else>
+        <div>Zaloguj się e-mailem
+          <input type="email" v-model="email">
+          <button @click="logIn()">Wchodzę</button>
+        </div>
+    </div>
+
+<!--    <div v-if="!email.includes('@')">W adresie brakuje znaku @ !</div>-->
+<!--    <div v-else-if="email.length < 10">Ale masz krótki adres! Twój adres ma {{ email.length }} znaki.</div>-->
+<!--    <div v-else-if="email.length < 15">Twój adres e-mail jest w sam raz - ma {{ email.length }} znaki.</div>-->
+<!--    <div v-else>Twój adres e-mail jest stanowczo za długi - ma {{ email.length }} znaki.</div>-->
   </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      email: 'dstasik@student.agh.edu.pl',
+      password: '',
+      loggedIn: false
+    };
+  },
+  methods: {
+    logIn() {
+      this.loggedIn = true;
+    },
+    logOut() {
+      this.loggedIn = false;
+    },
+    alertMyEmail() {
+      alert(this.email);
+    }
+  }
 }
 </script>
 
